4ff0fbed566fd7194eceae37b56d02beeb39fed9
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2023-03-12 20:04:48 +0900

    feat: Nota class

--- assignment2/src/main/java/assignments/assignment2/Nota.java
+++ assignment2/src/main/java/assignments/assignment2/Nota.java
@@ -3,21 +3,45 @@ package assignments.assignment2;
 import assignments.assignment1.NotaGenerator;
 
 public class Nota {
-	private static int idNota = 0;
+	private int idNota;
 	private String paket;
 	private Member member;
 	private int berat;
 	private String tanggalMasuk;
 	private int sisaHariPengerjaan;
-	private boolean isReady;
+	private boolean isReady = false;
     // TODO: tambahkan attributes yang diperlukan untuk class ini
-    public Nota(Member member, String paket, int berat, String tanggalMasuk) {
+    public Nota(int idNota, Member member, String paket, int berat, String tanggalMasuk) {
+		this.idNota = idNota;
     	this.member = member;
 		this.paket = paket;
 		this.berat = berat;
 		this.tanggalMasuk = tanggalMasuk;
+
+		switch (paket.toLowerCase()) {
+			case "reguler" -> this.sisaHariPengerjaan = 3;
+			case "fast" -> this.sisaHariPengerjaan = 2;
+			case "express" -> this.sisaHariPengerjaan = 1;
+		}
 		// TODO: buat constructor untuk class ini
     }
 
+	public void decSisaHariPengerjaan() {
+		if (this.sisaHariPengerjaan != 0) {
+			this.sisaHariPengerjaan--;
+			return;
+		}
+
+		this.isReady = true;
+	}
+
+	public boolean isReady() {
+		return isReady;
+	}
+
+	public int getIdNota() {
+		return this.idNota;
+	}
     // TODO: tambahkan methods yang diperlukan untuk class ini
+	
 }
