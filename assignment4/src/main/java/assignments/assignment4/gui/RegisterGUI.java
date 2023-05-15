030779a83624bfeb07ffd8dea3494520e11986ac
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2023-05-16 08:53:40 +0900

    feat: RegisterGUI

--- assignment4/src/main/java/assignments/assignment4/gui/RegisterGUI.java
+++ assignment4/src/main/java/assignments/assignment4/gui/RegisterGUI.java
@@ -1,5 +1,6 @@
 package assignments.assignment4.gui;
 
+import assignments.assignment1.NotaGenerator;
 import assignments.assignment3.LoginManager;
 import assignments.assignment3.user.Member;
 import assignments.assignment4.MainFrame;
@@ -42,6 +43,42 @@ public class RegisterGUI extends JPanel {
      * */
     private void initGUI() {
         // TODO
+        GridBagConstraints constraints = new GridBagConstraints();
+        
+        nameLabel = new JLabel("Masukkan nama Anda:");
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        mainPanel.add(nameLabel, constraints);
+
+        nameTextField = new JTextField();
+        constraints.gridy = 1;
+        mainPanel.add(nameTextField, constraints);
+
+        phoneLabel = new JLabel("Masukkan nomor handphone Anda:");
+        constraints.gridy = 2;
+        mainPanel.add(phoneLabel, constraints);
+
+        phoneTextField = new JTextField();
+        constraints.gridy = 3;
+        mainPanel.add(phoneTextField, constraints);
+
+        passwordLabel = new JLabel("Masukkan password Anda:");
+        constraints.gridy = 4;
+        mainPanel.add(passwordLabel, constraints);
+
+        passwordField = new JPasswordField();
+        constraints.gridy = 5;
+        mainPanel.add(passwordField, constraints);
+
+        registerButton = new JButton("Register");
+        registerButton.addActionListener((ActionEvent e) -> handleRegister());
+        constraints.gridy = 6;
+        mainPanel.add(registerButton, constraints);
+
+        backButton = new JButton("Back");
+        backButton.addActionListener((ActionEvent e) -> handleBack());
+        constraints.gridy = 7;
+        mainPanel.add(backButton, constraints);
     }
 
     /**
@@ -49,6 +86,8 @@ public class RegisterGUI extends JPanel {
      * Akan dipanggil jika pengguna menekan "backButton"
      * */
     private void handleBack() {
+        clearField();
+        MainFrame.getInstance().navigateTo(HomeGUI.KEY);
     }
 
     /**
@@ -57,5 +96,64 @@ public class RegisterGUI extends JPanel {
     * */
     private void handleRegister() {
         // TODO
+        String name = nameTextField.getText().trim();
+        String phoneNum = phoneTextField.getText().trim();
+        String pass = new String(passwordField.getPassword());
+
+        if (name.isEmpty() || phoneNum.isEmpty() || pass.isEmpty()) {
+            JOptionPane.showMessageDialog(
+                null, 
+                "Please fill in all of the fields", 
+                "Empty Field", 
+                JOptionPane.ERROR_MESSAGE
+            );
+            return;
+        }
+        
+        if (!NotaGenerator.isStringNumeric(phoneNum)) {
+            JOptionPane.showMessageDialog(
+                null, 
+                "Phone number has to be numeric!", 
+                "Invalid Phone Number", 
+                JOptionPane.ERROR_MESSAGE
+            );
+            phoneTextField.setText("");
+            return;
+        }
+
+        Member registeredMember = loginManager.register(name, phoneNum, pass);
+        if (registeredMember == null) {
+            JOptionPane.showMessageDialog(
+                null, 
+                String.format(
+                    "Member %s with phone number %s already exist!",
+                    name,
+                    phoneNum
+                ), 
+                "Registration Failed", 
+                JOptionPane.ERROR_MESSAGE
+            );
+            handleBack();
+            return;
+        }
+
+        JOptionPane.showMessageDialog(
+            null, 
+            String.format(
+                "Successfully created member with ID %s!",
+                registeredMember.getId()
+            ), 
+            "Registration Successfull", 
+            JOptionPane.INFORMATION_MESSAGE
+        );
+        
+        handleBack();
+        return;
+    }
+
+    private void clearField() {
+        nameTextField.setText("");
+        phoneTextField.setText("");
+        passwordField.setText("");
     }
 }
